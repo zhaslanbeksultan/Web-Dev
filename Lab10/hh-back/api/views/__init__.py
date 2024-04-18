f5389510e30f1575822f1d33012921e8a001d370
Beksultan <64122603+zhaslanbeksultan@users.noreply.github.com>
2024-04-19 04:08:00 +0500

    Delete Lab10 directory

--- Lab10/hh-back/api/views/__init__.py
+++
@@ -1,3 +0,0 @@
-# from .views1 import company_list, company_detail, company_vacancies, vacancy_list, vacancy_detail, vacancy_top_ten
-from .fbv import company_list, company_detail, company_vacancies, vacancy_list, vacancy_detail, vacancy_top_ten
-from .cbv import CompanyListAPIView
